1353ddf766e1db7c1859ce92445ce2e2e3f8d00d
Flavio Franco <flavio.win@gmail.com>
2021-02-25 16:05:28 -0300

    finalizando exercicios preliminares do dia 2 bloco 4

---
+++ bloco 4/dia_2/For/exemplo_1.js
@@ -0,0 +1,5 @@
+var numero = 7;
+
+for (var contador = 1; contador <= 9; contador++) {
+	console.log(numero * contador);
+}
---
+++ bloco 4/dia_2/For/exemplo_2.js
@@ -0,0 +1,6 @@
+var listaDeNomes = ["Joana", "Maria", "Lucas"];
+
+for (var i = 0; i < listaDeNomes.length; i++) {
+    var mensagem = "Boas vindas, " + listaDeNomes[i] + "!";
+    console.log(mensagem);
+}
