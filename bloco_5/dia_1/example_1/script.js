d940aa3da36a88b967184f1d28c28cb01c47057e
Flavio Franco <flavio.win@gmail.com>
2021-03-04 20:17:16 -0300

    finalizando exemplo 1 - getElementById()

---
+++ bloco_5/dia_1/example_1/script.js
@@ -0,0 +1,16 @@
+var paragraph = document.getElementById("paragraph");
+    paragraph.style.color = "green";
+
+
+/* 1- Recupere o elemento que contém o título da página e faça algo com ele, como alterá-lo 
+   para o nome do seu filme favorito. */
+
+document.getElementById("page-title").innerText = 'Titanic'
+
+/* 2- Agora recupere o segundo parágrafo e use sua criatividade para alterá-lo. */
+
+document.getElementById('second-paragraph').innerText = 'Um filme muito bom que eu amo de paixão!'
+
+/* 3- Por fim, recupere o subtítulo e altere-o também. */ 
+
+document.getElementById('subtitle').innerText = 'Leonardo Dicaprio'
