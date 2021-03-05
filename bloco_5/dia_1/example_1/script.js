68f9ec28afbc6c499a6f468466cb3de106f04c0a
Flavio Franco <flavio.win@gmail.com>
2021-03-05 10:59:45 -0300

    finalizando exemplos do bloco 5 dia 1

--- bloco_5/dia_1/example_1/script.js
+++ bloco_5/dia_1/example_1/script.js
@@ -14,3 +14,10 @@ document.getElementById('second-paragraph').innerText = 'Um filme muito bom que 
 /* 3- Por fim, recupere o subtítulo e altere-o também. */ 
 
 document.getElementById('subtitle').innerText = 'Leonardo Dicaprio'
+
+/* 
+document: guarda toda estrutura do html
+    document.getElementById("page-title").innerHTML - retorna toda o conteúdo da linha que contem o id page-title inclusive tags dentro da tag principal
+    document.getElementById("page-title").innerText - retorna toda o conteúdo da linha que contem o id page-title excluindo tags dentro da tag principal
+
+*/
