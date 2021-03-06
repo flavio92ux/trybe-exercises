74463e4e1c8f9f5d856a679f8845161c60009595
Flavio Franco <flavio.win@gmail.com>
2021-03-05 22:31:57 -0300

    finalizando primeiro exemplo

--- bloco_5/dia_2/script.js
+++
@@ -1,5 +0,0 @@
-//1. Acesse o elemento elementoOndeVoceEsta.
-document.querySelector('#elementoOndeVoceEsta');
-
-//2.Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
-document.querySelector('#elementoOndeVoceEsta').parentNode
