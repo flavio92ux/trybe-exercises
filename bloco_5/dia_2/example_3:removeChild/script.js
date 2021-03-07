175696fe334a40ab5c888b5a3e02b85d5b987d2c
Flavio Franco <flavio.win@gmail.com>
2021-03-07 00:06:33 -0300

    finalizando exercicios do dia 2 do bloco 5

--- bloco_5/dia_2/example_3:removeChild/script.js
+++ bloco_5/dia_2/example_3:removeChild/script.js
@@ -9,7 +9,7 @@ A partir desse filho criado, acesse terceiroFilho.
 let ingredientItems = [
     'primeiro item',
     'segundo item',
-    'terceiro item',
+    'terceiro',
     'quarto item',
     'quinto item'
 ];
@@ -21,6 +21,20 @@ for (let index = 0; index < ingredientItems.length; index += 1) {
 
     let itemListCreator = document.createElement('li');
     itemListCreator.innerText = itens;
+    itemListCreator.className = 'my_itens';
 
     ingredientList.appendChild(itemListCreator);
+}
+
+//Remover terceiro item;
+
+let myListCreated = document.querySelectorAll('.my_itens');
+console.log(myListCreated);
+
+for (let index = 0; index < myListCreated.length; index += 1) {
+    let item = myListCreated[index];
+
+    if (item.innerText.includes('terceiro')) {
+        ingredientList.removeChild(item);
+    }
 }
